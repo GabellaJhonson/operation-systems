a7d39b49e4c1357f4526c6605fa0b475af03eeb7
GabellaJhonson <102244000+GabellaJhonson@users.noreply.github.com>
2022-10-26 11:08:43 +0300

    Create Solution.cpp

---
+++ PrimeNum/Solution.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <mutex>
+using namespace std;
+int iter = 0;
+mutex m;
+bool checkPrimeNumber(int n) {
+    bool isPrimeNumber = true;
+    if (n == 0 || n == 1) {
+        isPrimeNumber = false;
+    }
+    else {
+        for (int i = 2; i <= n / 2; ++i) {
+            if (n % i == 0) {
+                isPrimeNumber = false;
+                break;
+            }
+        }
+    }
+    return isPrimeNumber;
+}
+void PrimeCheck(vector <int>& vec, vector <bool>& res) {
+    while (1) {
+        unique_lock<mutex> guard(m);
+        if (iter == vec.size()) {
+            return;
+        }
+        else {
+            iter++;
+        }
+        guard.unlock();
+        if(checkPrimeNumber(vec[iter - 1]))
+            res[iter - 1] = 1;
+    }
+    
+}
+int main() {
+    int n;
+    cout << "Enter size of array: " << endl;
+    cin >> n;
+    cout << "Enter elements: " << endl;
+    vector <int> vec(n);
+    vector <bool> res(n);
+    for (size_t i = 0; i < vec.size(); i++)
+    {
+        cin >> vec[i];
+    }
+   
+    cout << endl;
+    cout << "Enter count of threads: ";
+    int sz;
+    cin >> sz;
+    vector <thread> th;
+    for (int i = 0; i < sz; i++)
+    {
+        //th.emplace_back(power, vec, i);  // на паре так делали
+        th.emplace_back([&vec, &res]() {PrimeCheck(vec, res); });
+    }
+    for (int i = 0; i < sz; i++)
+    {
+        th[i].join();
+    }
+    for (size_t i = 0; i < res.size(); i++)
+    {
+        cout << res[i] << " ";
+    }
+}
