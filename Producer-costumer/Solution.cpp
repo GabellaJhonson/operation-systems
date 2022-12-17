9857e1b99f2f830d33d96513ec1440d24d569564
Zahar Statkevich <102244000+GabellaJhonson@users.noreply.github.com>
2022-12-17 09:24:13 +0300

    Create Solution.cpp

---
+++ Producer-costumer/Solution.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <mutex>
+using namespace std;
+int signal = 0;
+mutex m;
+condition_variable cv;
+bool checkPrimeNumber(int n) {
+    bool isPrimeNumber = true;
+    if (n == 0 || n == 1) {
+        isPrimeNumber = false;
+    }
+    else {
+        for (int i = 2; i <= n / 2; ++i) {
+            if (n % i == 0) {
+                isPrimeNumber = false;
+                break;
+            }
+        }
+    }
+    return isPrimeNumber;
+}
+void FindPrime(int n) {
+    for (int i = n;; i++) {
+        if (checkPrimeNumber(i)) {
+            signal = i;
+            cv.notify_all();
+            return;
+        }
+    }
+}
+void Print(){
+    unique_lock<mutex> guard(m);
+    cv.wait(guard);
+    cout << signal;
+}
+int main() {
+    int n;
+    cin >> n;
+    thread first(FindPrime, n);
+    thread second(Print);
+    first.join();
+    second.join();
+}
