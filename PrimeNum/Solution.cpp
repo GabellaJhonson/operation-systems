c2ccae78ea94ab2982ab548fbb9f50ce9bdc83d9
Zahar Statkevich <102244000+GabellaJhonson@users.noreply.github.com>
2022-12-17 09:30:50 +0300

    Update Solution.cpp

--- PrimeNum/Solution.cpp
+++ PrimeNum/Solution.cpp
@@ -55,7 +55,7 @@ int main() {
     vector <thread> th;
     for (int i = 0; i < sz; i++)
     {
-        th.emplace_back(ref(vec), ref(res));  // на паре так делали
+        th.emplace_back(PrimeCheck, ref(vec), ref(res));  // на паре так делали
         //th.emplace_back([&vec, &res]() {PrimeCheck(vec, res); });
     }
     for (int i = 0; i < sz; i++)
